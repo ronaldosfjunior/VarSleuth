d4cd95a15969c94c8076dfb737b294af7a11c2d2
ronaldosfjunior <ronaldoj@stanford.edu>
2023-07-20 18:20:56 -0700

    Initial commit

---
+++ R/VarSleuth_init.R
@@ -0,0 +1,8 @@
+# Set your Git username
+usethis::use_git_config(user.name = "ronaldosfjunior", global = TRUE)
+
+# Set your Git email
+usethis::use_git_config(user.email = "ronaldoj@stanford.edu", global = TRUE)
+
+# Initiate a Git repository
+use_git()
